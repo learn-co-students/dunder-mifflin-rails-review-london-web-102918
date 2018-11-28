bfa953c7219e368acb9919909cd98431f423a21a
Paul-Cl-ark <clark.paul123@gmail.com>
2018-11-28 09:56:48 +0000

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,15 @@
 class DogsController < ApplicationController
+
+  def index
+    if !params[:order].nil? && !params[:order][:descending].nil?
+      @dogs = Dog.sort_by_num_of_owners.reverse
+    else
+      @dogs = Dog.sort_by_num_of_owners
+    end
+  end
+
+  def show
+    @dog = Dog.find(params[:id])
+  end
+
 end
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,2 +1,13 @@
 class Dog < ApplicationRecord
+    has_many :employees
+
+
+  def num_of_owners
+    employees.length
+  end
+
+  def self.sort_by_num_of_owners
+    Dog.all.sort_by{|r| r.num_of_owners}
+  end
+
 end
