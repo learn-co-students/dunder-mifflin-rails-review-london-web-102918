7161760f51103e233466fca59e0c04d1d2ba81a5
github username <github email address>
2018-11-29 12:06:49 +0000

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,28 @@
 class DogsController < ApplicationController
+  before_action :find_dog, only:[:show, :edit]
+
+  def index
+    if !params[:order].nil? && !params[:order][:descending].nil?
+      @dogs = Dog.sort_by_num_of_employees.reverse
+    else
+      @dogs = Dog.sort_by_num_of_employees
+    end
+  end
+
+  def show
+  end
+
+  def new
+    @dog = Dog.new
+  end
+
+  def edit
+  end
+
+  private
+
+  def find_dog
+    @dog = Dog.find(params[:id])
+  end
+
 end
